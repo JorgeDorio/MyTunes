5ac3c26b35c527b8f8bece6aeab9fb11939db3f6
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-08-23 15:29:15 -0300

    Album page

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,28 +1,37 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
-import getMusics from '../helpers/musicsAPI'; 
+import getMusics from '../helpers/musicsAPI';
 
 const Album = () => {
-	const id =  document.location.pathname.split('/')[2];
-	const  [currentAlbum,  setCurrentAlbum] = useState([]);
-	const [albumDetails,  setAlbumDetails] = useState({collectionName: ''});
-	const getAlbum = async () => {
-		const album  = await getMusics(id);
-		setAlbumDetails(album.shift());
-		setCurrentAlbum(album);
-		console.log(currentAlbum[0]);
-	};
+  const id = document.location.pathname.split('/')[2];
+  const [currentAlbum, setCurrentAlbum] = useState([]);
+  const [albumDetails, setAlbumDetails] = useState();
+  const getAlbum = async () => {
+    const album = await getMusics(id);
+    setAlbumDetails(album.shift());
+    setCurrentAlbum(album);
+  };
 
-	useEffect(() => {
-		getAlbum();
-	},[]);
+  useEffect(() => {
+    getAlbum();
+  }, []);
 
-	return (
-		<div className="main-frame">
-			<Header/> 
-			{currentAlbum && <p>render</p>}
-		</div>
-	);
+  return (
+    <div className="main-frame">
+      <Header />
+      {albumDetails && <div className='albumArtworkFrame'><img className='albumArtwork' src={albumDetails.artworkUrl100} /><p>{albumDetails.artistName}</p></div>}
+      <div className='audioBody'>
+        {currentAlbum.map((music) => (
+          <div className='trackBody'>
+            {music.trackName}
+            {/* <input id="heart" type="checkbox" /> */}
+            {/* <label for="heart">❤</label> */}
+            <span><audio src={music.previewUrl} controls className='audioTrack' /></span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Album;
